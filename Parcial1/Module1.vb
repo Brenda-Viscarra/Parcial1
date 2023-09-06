21e1edde38a86364116f7bce93bc08682a2f2e8c
HP <HP@DESKTOP-HOPUCHT>
2023-09-05 18:37:07 -0600

    Agregar archivos de proyecto.

---
+++ Parcial1/Module1.vb
@@ -0,0 +1,56 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Consulta de Gentilicios y Tipos de Animales")
+
+        Dim paises() As String = {"El Salvador", "México", "Argentina", "Alemania", "Rusia"}
+        Dim gentilicios() As String = {"salvadoreño", "mexicano", "argentino", "Alemán", "Rusos"}
+        Dim animales() As String = {"Lobo", "León", "Águila", "Tigre", "Caballo"}
+        Dim especies() As String = {"Canina", "Felina", "Ave", "Felina", "Equina"}
+
+        ' Mostrar un menú de opciones
+        Console.WriteLine("Seleccione una opción:")
+        Console.WriteLine("1. Consultar Gentilicio")
+        Console.WriteLine("2. Consultar Tipo de Animal")
+        Dim opcion As Integer = Integer.Parse(Console.ReadLine())
+
+        If opcion = 1 Then
+            Console.WriteLine("Ingrese un país:")
+            Dim paisBuscado As String = Console.ReadLine()
+
+            Dim encontrado As Boolean = False
+            For i As Integer = 0 To paises.Length - 1
+                If paises(i) = paisBuscado Then
+                    Console.WriteLine("El gentilicio de " & paisBuscado & " es " & gentilicios(i))
+                    encontrado = True
+                    Exit For
+                End If
+            Next
+
+            If Not encontrado Then
+                Console.WriteLine("País no encontrado.")
+            End If
+        ElseIf opcion = 2 Then
+            Console.WriteLine("Ingrese una especie:")
+            Dim especieBuscada As String = Console.ReadLine()
+
+            Dim encontrado As Boolean = False
+            For i As Integer = 0 To especies.Length - 1
+                If especies(i) = especieBuscada Then
+                    Console.WriteLine("El tipo de animal de la especie " & especieBuscada & " es " & animales(i))
+                    encontrado = True
+                    Exit For
+                End If
+            Next
+
+            If Not encontrado Then
+                Console.WriteLine("Especie no encontrada.")
+            End If
+        Else
+            Console.WriteLine("Opción no válida.")
+        End If
+
+        Console.WriteLine("Ejecución del programa terminada.")
+    End Sub
+
+End Module
